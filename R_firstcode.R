84276fe4cfc010c2318b995b9505d00c0922c33b
CorradoDalla1999 <127426218+CorradoDalla1999@users.noreply.github.com>
2023-03-10 18:00:35 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -8,7 +8,16 @@ library(raster)
 # import data, setting the working directory
 # setwd("C:/lab/") # windows
 
-l2011 <-brick("p224r63_2011_masked")    #tutte le volte che si esce da R si devono usare le virgolette
-plot (l2011)
-cl <- colorRamplette(c("red","orange","yelow")) (100)
-clb <- colorRampPalette(c("blu scuro", "blu", "azzurro")) (100) #per cambiare i colori, 100 è il numero delle sfumature
+library(raster)
+setwd("C:/Telerilevamento_lab")
+getwd()
+l2011<-brick("p224r63_2011_masked.grd")
+plot(l2011)
+cl <- colorRampPalette(c("red","orange","yellow")) (100)    #per cambiare i colori, abbiamo 3 colori di base e le diverse sfumature
+plot(l2011,col=cl) 
+
+#plotting one element
+plot(l2011[[4]], col=cl)
+plot(l2011$B4_sre, col=cl)     #il dollarro lega dei pezzi fra loro
+nir <- l2011 [[4]]  #or: nir <- l2011$B4_sre
+plot(nir, col=cl)
