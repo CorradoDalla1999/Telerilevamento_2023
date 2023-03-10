2c832ff63f5f1776b8bee5d1e57d8ffb5134b141
CorradoDalla1999 <127426218+CorradoDalla1999@users.noreply.github.com>
2023-03-10 17:57:10 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -4,3 +4,11 @@
 install.packages("raster")
 
 library(raster)
+
+# import data, setting the working directory
+# setwd("C:/lab/") # windows
+
+l2011 <-brick("p224r63_2011_masked")    #tutte le volte che si esce da R si devono usare le virgolette
+plot (l2011)
+cl <- colorRamplette(c("red","orange","yelow")) (100)
+clb <- colorRampPalette(c("blu scuro", "blu", "azzurro")) (100) #per cambiare i colori, 100 è il numero delle sfumature
