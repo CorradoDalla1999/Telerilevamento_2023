5eed4f9843d901c5ea96232c4b0b9dd8751bc7ec
CorradoDalla1999 <127426218+CorradoDalla1999@users.noreply.github.com>
2023-03-17 17:51:12 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -22,6 +22,7 @@ plot(l2011$B4_sre, col=cl)                    #il dollarro lega dei pezzi fra lo
 nir <- l2011 [[4]]                            #or: nir <- l2011$B4_sre
 plot(nir, col=cl)
 
+
 # giorno 16/03/23
 cl <- colorRampPalette(c("darkorchid","cyan","bisque","darkmagenta","cyan4")) (200)    #cambio nuovamente i colori scegliendo 5 colori diversi e mettendo 200 sumature e le ho attribuite a cl
 plot(l2011, col=cl)                                                                    #Plot02
@@ -76,3 +77,32 @@ plot(l2011[[1]], col=cln)
 
 #OTTERO' COME RISULTATO LE QUATTRO MAPPE PLOTTATE CON BASE CROMATICA A DIFFERENTI COLORI (INFRAROSSO,ROSSO,VERDE E BLU) (Plot08)
 
+
+# giorno 17/03/23
+# Componenti RGB (red,green,blue), come prima cosa caricheremo un pacchetto raster
+library(raster)
+setwd("C:/Telerilevamento_lab")
+l2011 <- brick ("p224r63_2011_masked.grd")
+#faremo una immagine a colori naturali, associamo ad ogni componente la banda corrispondente.
+#RGB plotting, plottiamo un raster con le tre componenti di colore.Per aver informazioni sulle funzioni digita prima della funzione stessa: ?     Alla componente 3 diamo la banda numero 3, alla verde la 2, alla blu la 1.
+#strecht per allargare il più possibile la scala di colore lineare.
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin") #plot09
+#Ora evidenziamo la vegetazione (usiamo una delle bande che identifichi al meglio la vegetazione, useremo l'infrarosso (banda numero4) per evidenziare la vegetazione)
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin") #plot10  #tutto quello che riflette molto nella banda numero 4 (infrarossso) diventerà rosso.
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin") #plot11  #per mostrare l'infrarosso in verde  enon con il rosso
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")  #plot12 per mostrare l'infrarosso in blue
+
+#Multiframe with natural and false colours per vedere i due plot confrontati.
+par(mfrow=c(2,1))
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")   #plot13
+
+#Histogram stretching Hist al posto di lin
+par(mfrow=c(2,1))
+plotRGB(l2011, r=3, g=2, b=1, stretch="Hist")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Hist")  #plot14
+
+#Lin vs Hist
+par(mfrow=c(2,1))
+plotRGB(l2011, r=4, g=3, b=2, stretch="Hist")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")   #plot15
