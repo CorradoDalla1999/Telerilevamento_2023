5158d28521a5dcf5d7ec80fe92313390ce9ed7ac
CorradoDalla1999 <127426218+CorradoDalla1999@users.noreply.github.com>
2023-04-13 17:26:53 +0200

    Create R_code_classification.R

---
+++ R_code_classification.R
@@ -0,0 +1,6 @@
+#Classification of remote sensing data via RStoolbox
+
+#installare devtools che permetterà di scaricare i pacchetti fuori dal CRAN
+install.packages("devtools")
+library(devtools)
+install_github("bleutner/RStoolbox")
