0552cb32ccf2efc75eb131bfcf449b70cc005398
CorradoDalla1999 <127426218+CorradoDalla1999@users.noreply.github.com>
2023-05-16 16:25:46 +0200

    Update R_code_lab.R

--- R_code_lab.R
+++ R_code_lab.R
@@ -2,14 +2,6 @@
 # install.packages("ncdf4")
 library(ncdf4)
 library(raster)
-
-# 2- Working directory
-setwd("~/lab/") # Linux
-# setwd("C:/lab/") # Windows
-# setwd("/Users/name/Desktop/lab/") # Mac
-
-# 3- Importing imagery
+setwd("C:/Telerilevamento_lab")
 sc <- raster("c_gls_SSM1km_202305030000_CEURO_S1CSAR_V1.2.1.nc")
-
-# 4- Plotting the image
-plot(sc)
+plot(sc)   #plot73
