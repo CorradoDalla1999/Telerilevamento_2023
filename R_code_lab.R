6eada0aa4a1ae2ce2b5f7a2f809cf6a37d444b18
CorradoDalla1999 <127426218+CorradoDalla1999@users.noreply.github.com>
2023-05-16 18:53:02 +0200

    Delete R_code_lab.R

--- R_code_lab.R
+++
@@ -1,7 +0,0 @@
-# R cocde per scaricare e visualizzare data-Copernicus
-# install.packages("ncdf4")
-library(ncdf4)
-library(raster)
-setwd("C:/Telerilevamento_lab")
-sc <- raster("c_gls_SSM1km_202305030000_CEURO_S1CSAR_V1.2.1.nc")
-plot(sc)   #plot73
